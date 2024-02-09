25625897290c32873eee07185fd38b1a79c807fc
jmerheb <87794648+jmerheb@users.noreply.github.com>
2024-02-08 21:30:19 -0500

    Update twilio_sms.py

--- build/twilio_sms.py
+++ build/twilio_sms.py
@@ -3,8 +3,8 @@ import os
 from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = 'AC8c072f0291fa7c463c78a833fc849345'
-auth_token = '8a4ac6cae00b558cf3c0698b04b3b2db'
+account_sid = # Add this from twilio
+auth_token = # Add this from twilio
 
 def send_twilio_message(text_message, recipient):
   client = Client(account_sid, auth_token)
@@ -12,7 +12,7 @@ def send_twilio_message(text_message, recipient):
   print(recipient)
   message = client.messages.create(
     body=text_message,
-    from_="+18669365256",
+    from_= # Add this from twilio ,
     to= recipient 
   )
   return message.sid 
